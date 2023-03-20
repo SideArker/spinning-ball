8f136a2ec12b7758f22dd1b120f76dd0395670b2
Kapelanq <114695646+Kapelanq@users.noreply.github.com>
2023-03-20 11:06:25 +0100

    BorderMenu image

---
+++ Assets/Scripts/Scratcher.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scratcher : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
